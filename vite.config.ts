1f87184e4654872efa44a1c51a0138637f481c04
Leul tesfu <leultesfu24@gmail.com>
2026-06-18 12:04:20 +0300

    test-mode for hosting the ui

--- vite.config.ts
+++ vite.config.ts
@@ -45,7 +45,8 @@ export default defineConfig({
     strictPort: false,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5000",
+        // target: "http://127.0.0.1:5000",
+        target: "https://backend-hl-org.onrender.com/",
         changeOrigin: true,
         secure: false,
       },
